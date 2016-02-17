252bff1332a919b25dbff72b12d0230746ab52d6
Adamino <adamlars90@gmail.com>
2016-02-17 09:46:27 +0100

    Finished alpha

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 var turnusApp = angular
 .module('turnusApp', ['ui.router', 'ngResource', 'lbServices'])
 .config(function(LoopBackResourceProvider) {
-    LoopBackResourceProvider.setUrlBase('http://127.0.0.1:3000/api')
+    LoopBackResourceProvider.setUrlBase('https://turnus-server.herokuapp.com/api')
   });
